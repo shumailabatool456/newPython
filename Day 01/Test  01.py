f59818066a2f8a9fa1f2ec7ede83f6fa67396377
shumailabatool456 <shumailabatool896@gmail.com>
2025-08-07 17:56:05 +0500

    Assesement _Test Day 01

---
+++ Day 01/Test  01.py
@@ -0,0 +1,11 @@
+
+print("Cool breeze blow and refresh our mind.")
+print(input("What is your name?\n"))
+
+input("What is your name?")
+color = input("What is your favourite color?\n")
+print(f"Wow! {color} is a nice color")
+
+name = input("Enter your name: " )
+print("Hello" + " " + name)
+
---
+++ Day 01/Test 1.1.py
@@ -0,0 +1,5 @@
+
+print("WElcome to the Band Name Generator")
+city = input("In which city you grew up?\n")
+pet = input("What is your pet's name?\n")
+print(" Your band name could be " + str(city) + " " + str(pet))
